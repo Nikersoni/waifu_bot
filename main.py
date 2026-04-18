9c7bdb455c5773b27248f571abdc1bac6ac71bac
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 22:02:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
 import asyncio
-import logging
-
 from aiogram import Bot, Dispatcher
-
 from config import BOT_TOKEN
 from db import init_db
 
-# handlers
-from handlers import start, card, profile, inventory, bonus, top, institute
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-# регистрация роутеров
-dp.include_router(start.router)
-dp.include_router(card.router)
-dp.include_router(profile.router)
-dp.include_router(inventory.router)
-dp.include_router(bonus.router)
-dp.include_router(top.router)
-dp.include_router(institute.router)
-
+from handlers import start, card, profile, inventory, bonus, top, help, institute
 
 async def main():
-    # 🟢 инициализация PostgreSQL + таблиц
     await init_db()
 
-    print("🚀 Bot started successfully")
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
 
-    # запуск бота
+    dp.include_router(start.router)
+    dp.include_router(card.router)
+    dp.include_router(profile.router)
+    dp.include_router(inventory.router)
+    dp.include_router(bonus.router)
+    dp.include_router(top.router)
+    dp.include_router(help.router)
+    dp.include_router(institute.router)
+
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
