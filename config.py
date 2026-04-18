602eeb1fb1b76f9a3b98387aafcf5ef7673aba13
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:03:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "8648276698:AAEPegOqD3pThyls1Qx5A3TkIQAH90AMtoA"
 
 
-DATABASE_URL = "postgresql://postgres:NEW_PASSWORD@postgres.railway.internal:5432/railway"
+DATABASE_URL = "postgresql://postgres:JgQBlskznryrRGKgcAywKKzXyrPtRmnM@postgres.railway.internal:5432/railway"
