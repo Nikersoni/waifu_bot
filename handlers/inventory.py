bcd5810a6fe5abe209f575e42c028b1a4e87ef49
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:07:11 +0300

    Update inventory.py

--- handlers/inventory.py
+++ handlers/inventory.py
@@ -1,1 +1,8 @@
+from aiogram import Router, F
+from aiogram.types import Message
 
+router = Router()
+
+@router.message(F.text.in_(["🎒 Инвентарь", "инв", "инвентарь"]))
+async def inventory(msg: Message):
+    await msg.answer("🎒 Инвентарь пока в разработке")
