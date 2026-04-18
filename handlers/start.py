3bfa3e18217018d7ed3b7e6c7fd0a697a629ffd8
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 22:15:54 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,13 +1,22 @@
 from aiogram import Router, types
 from keyboards.main_menu import main_menu
+from db import get_or_create_user
 
 router = Router()
 
+
 @router.message(lambda m: m.chat.type == "private")
 async def start(msg: types.Message):
+
+    # 🧠 создаём пользователя в БД
+    await get_or_create_user(
+        msg.from_user.id,
+        msg.from_user.username or "unknown"
+    )
+
     text = (
         "👋 Добро пожаловать в Waifu World!\n\n"
-        "🎴 Собирай вайфу, открывай карточки и развивай коллекцию\n\n"
+        "🎴 Открывай карточки, собирай коллекцию и прокачивайся\n\n"
         "👉 Нажми кнопку ниже или напиши 'карта'"
     )
 
