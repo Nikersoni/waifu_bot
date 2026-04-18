45fce278deb01ddd628953f2cdd177d3f08973d4
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:13:36 +0300

    Update inventory.py

--- handlers/inventory.py
+++ handlers/inventory.py
@@ -1,8 +1,28 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from db import cursor
 
 router = Router()
 
-@router.message(F.text.in_(["🎒 Инвентарь", "инв", "инвентарь"]))
-async def inventory(msg: Message):
-    await msg.answer("🎒 Инвентарь пока в разработке")
+
+@router.message(F.text.in_(["инв", "инвентарь", "🎒 Инвентарь"]))
+async def inv(msg: Message):
+
+    cursor.execute("""
+    SELECT card_name, rarity, count
+    FROM inventory
+    WHERE user_id=?
+    """, (msg.from_user.id,))
+
+    rows = cursor.fetchall()
+
+    if not rows:
+        await msg.answer("Пусто")
+        return
+
+    text = "🎒 Инвентарь\n\n"
+
+    for r in rows:
+        text += f"{r[0]} x{r[2]}\n"
+
+    await msg.answer(text)
