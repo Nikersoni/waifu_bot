dcb9187216821fb5d1ca08daeb1773287ab7a043
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:59:31 +0300

    Update card.py

--- handlers/card.py
+++ handlers/card.py
@@ -35,12 +35,15 @@ async def card(msg: Message):
         create_user(msg.from_user.id, msg.from_user.username)
         user = get_user(msg.from_user.id)
 
+    # ⏳ КД
     if not check_cd(user[4], CARD_COOLDOWN):
         await msg.answer("⏳ Карточка доступна раз в 24 часа")
         return
 
+    # 🎴 выпадение
     card_name, rarity = roll_card()
 
+    # 📦 в инвентарь
     cursor.execute("""
     INSERT INTO inventory (user_id, card_name, rarity, count)
     VALUES (?,?,?,1)
@@ -48,10 +51,21 @@ async def card(msg: Message):
     DO UPDATE SET count = count + 1
     """, (msg.from_user.id, card_name, rarity))
 
+    # 💍 делаем активной вайфу
+    cursor.execute("""
+    UPDATE users
+    SET active_name=?,
+        active_rarity=?,
+        active_level=1,
+        active_hp=100
+    WHERE user_id=?
+    """, (card_name, rarity, msg.from_user.id))
+
     conn.commit()
 
     update_user_time(msg.from_user.id, "last_card", int(time.time()))
 
+    # ✨ вывод
     await msg.answer(
 f"""✨ ТЕБЕ ВЫПАЛА ВАЙФУ!
 
