3376ce7b769b4a0edfc8a5d3ffc68a649c852163
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:36:14 +0300

    Update card.py

--- handlers/card.py
+++ handlers/card.py
@@ -1,37 +1,59 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from db import get_user, update_user_time, create_user, cursor, conn
-from config import CARD_COOLDOWN
+import time
+
+from db import cursor, conn, get_user, create_user, update_user_time
 from services.cards import roll_card
 from services.cooldown import check_cd
-import time
+from config import CARD_COOLDOWN
 
 router = Router()
 
-@router.message(F.text.in_(["🎴 Карта", "карта"]))
+RARITY_EMOJI = {
+    "common": "⚪",
+    "rare": "🟢",
+    "epic": "🔵",
+    "legend": "🟣",
+    "myth": "🟡"
+}
+
+RARITY_NAME = {
+    "common": "Обычная",
+    "rare": "Редкая",
+    "epic": "Эпическая",
+    "legend": "Легендарная",
+    "myth": "Мифическая"
+}
+
+
+@router.message(F.text.in_(["карта", "🎴 Карта"]))
 async def card(msg: Message):
+
     user = get_user(msg.from_user.id)
 
     if not user:
         create_user(msg.from_user.id, msg.from_user.username)
         user = get_user(msg.from_user.id)
 
-    last = user[4]
-
-    if not check_cd(last, CARD_COOLDOWN):
-        await msg.answer("⏳ Карточку можно раз в 24 часа")
+    if not check_cd(user[4], CARD_COOLDOWN):
+        await msg.answer("⏳ Карточка доступна раз в 24 часа")
         return
 
     card_name, rarity = roll_card()
 
     cursor.execute("""
-    INSERT INTO inventory (user_id, card_name, count)
-    VALUES (?,?,1)
+    INSERT INTO inventory (user_id, card_name, rarity, count)
+    VALUES (?,?,?,1)
     ON CONFLICT(user_id, card_name)
-    DO UPDATE SET count=count+1
-    """, (msg.from_user.id, card_name))
+    DO UPDATE SET count = count + 1
+    """, (msg.from_user.id, card_name, rarity))
 
     conn.commit()
+
     update_user_time(msg.from_user.id, "last_card", int(time.time()))
 
-    await msg.answer(f"🎁 Ты получил:\n\n{card_name}")
+    await msg.answer(
+f"""✨ ТЕБЕ ВЫПАЛА ВАЙФУ!
+
+{RARITY_EMOJI[rarity]} {card_name}  [{RARITY_NAME[rarity]}]"""
+    )
