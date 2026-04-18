f19106556af89342e2e75f657d5f6dcd1c32bd78
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:18:47 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -2,6 +2,7 @@ from aiogram import Router, types
 
 router = Router()
 
-@router.message(lambda m: m.text and m.text.lower() in ["профиль","profile","проф","акк"])
+
+@router.message(lambda m: m.text and m.text.lower().strip() in ["профиль", "👤 профиль"])
 async def profile(msg: types.Message):
-    await msg.answer("👤 Профиль игрока (заглушка)")
+    await msg.answer("👤 Профиль (заглушка)")
