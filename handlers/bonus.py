547dacd8bbdaa24c9d886d34770e3f4cb9eda231
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:19:38 +0300

    Update bonus.py

--- handlers/bonus.py
+++ handlers/bonus.py
@@ -2,6 +2,7 @@ from aiogram import Router, types
 
 router = Router()
 
-@router.message(lambda m: m.text and m.text.lower() in ["бонус","daily","ежедневка"])
+
+@router.message(lambda m: m.text and m.text.lower().strip() in ["бонус", "🎁 бонус"])
 async def bonus(msg: types.Message):
     await msg.answer("🎁 Бонус (заглушка)")
