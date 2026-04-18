e950e34c7ed423bb49354a6d7297ddc39e686a02
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 13:33:30 +0300

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,1 +1,10 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+main_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🎴 Карта"), KeyboardButton(text="👤 Профиль")],
+        [KeyboardButton(text="🎒 Инвентарь"), KeyboardButton(text="🎁 Бонус")],
+        [KeyboardButton(text="🏆 Топ")]
+    ],
+    resize_keyboard=True
+)
