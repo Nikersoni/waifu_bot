db179c18210ee4969ed072e9caa05aa6c772c187
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:47:13 +0300

    Update card.py

--- handlers/card.py
+++ handlers/card.py
@@ -3,6 +3,6 @@ from aiogram import Router, types
 router = Router()
 
 
-@router.message(lambda m: m.text and m.text.lower().strip() in ["карта", "🎴 карта"])
+@router.message(lambda m: m.text and m.text.lower().strip() in ["карта", "🎴 карты"])
 async def card(msg: types.Message):
     await msg.answer("🚧 карты в разработке")
