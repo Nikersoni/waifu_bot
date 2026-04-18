f443754981e5d3732082b9c24aad0e519272df2c
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:14:20 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,21 +1,25 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from db import get_user
+from db import get_user, cursor
 
 router = Router()
 
-@router.message(F.text.in_(["👤 Профиль", "профиль"]))
+
+@router.message(F.text.in_(["профиль", "👤 Профиль"]))
 async def profile(msg: Message):
+
     user = get_user(msg.from_user.id)
 
-    if not user:
-        await msg.answer("Нет профиля")
-        return
+    cursor.execute("SELECT COUNT(*) FROM inventory WHERE user_id=?",
+                   (msg.from_user.id,))
+
+    cards = cursor.fetchone()[0]
 
     await msg.answer(
 f"""👤 {msg.from_user.username}
 
 💎 Алмазы: {user[2]}
 🧪 Пыль: {user[3]}
+🎴 Карточек: {cards}
 """
     )
