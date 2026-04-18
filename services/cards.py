34175c7bee3b954565bee285180b083514836c32
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:11:28 +0300

    Update cards.py

--- services/cards.py
+++ services/cards.py
@@ -1,25 +1,34 @@
 import random
 
-cards = [
+CARDS = [
     ("Луна", "common"),
-    ("Мика", "rare"),
+    ("Мика", "common"),
+    ("Сора", "common"),
+    ("Рин", "common"),
+
+    ("Аой", "rare"),
+    ("Мэй", "rare"),
+
     ("Ая", "epic"),
+    ("Хикари", "epic"),
+
     ("Хината", "legend"),
     ("Акари", "myth")
 ]
+
 
 def roll_card():
     r = random.randint(1, 100)
 
     if r <= 50:
-        pool = [c for c in cards if c[1] == "common"]
+        pool = [c for c in CARDS if c[1] == "common"]
     elif r <= 80:
-        pool = [c for c in cards if c[1] == "rare"]
+        pool = [c for c in CARDS if c[1] == "rare"]
     elif r <= 95:
-        pool = [c for c in cards if c[1] == "epic"]
+        pool = [c for c in CARDS if c[1] == "epic"]
     elif r <= 99:
-        pool = [c for c in cards if c[1] == "legend"]
+        pool = [c for c in CARDS if c[1] == "legend"]
     else:
-        pool = [c for c in cards if c[1] == "myth"]
+        pool = [c for c in CARDS if c[1] == "myth"]
 
     return random.choice(pool)
