fff414bac799d990e3d7612570a56fab32832761
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:41:17 +0300

    Update card.py

--- handlers/card.py
+++ handlers/card.py
@@ -5,4 +5,4 @@ router = Router()
 
 @router.message(lambda m: m.text and m.text.lower().strip() in ["карта", "🎴 карта"])
 async def card(msg: types.Message):
-    await msg.answer("🎴 Ты открыл карту (заглушка)")
+    await msg.answer("🚧 карты в разработке")
