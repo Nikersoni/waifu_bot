6b48a8d0ffc38b8208ed1587720e385aab09dc8d
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 14:58:53 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
 import sqlite3
+import os
+
+os.makedirs("data", exist_ok=True)
 
 conn = sqlite3.connect("data/bot.db", check_same_thread=False)
 cursor = conn.cursor()
@@ -12,7 +15,12 @@ def init_db():
         diamonds INTEGER DEFAULT 0,
         dust INTEGER DEFAULT 0,
         last_card INTEGER DEFAULT 0,
-        last_bonus INTEGER DEFAULT 0
+        last_bonus INTEGER DEFAULT 0,
+
+        active_name TEXT DEFAULT NULL,
+        active_rarity TEXT DEFAULT NULL,
+        active_level INTEGER DEFAULT 1,
+        active_hp INTEGER DEFAULT 100
     )
     """)
 
@@ -35,11 +43,17 @@ def get_user(user_id):
 
 
 def create_user(user_id, username):
-    cursor.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (?,?)",
-                   (user_id, username))
+    cursor.execute("""
+    INSERT OR IGNORE INTO users (user_id, username)
+    VALUES (?,?)
+    """, (user_id, username))
     conn.commit()
 
 
 def update_user_time(user_id, field, value):
-    cursor.execute(f"UPDATE users SET {field}=? WHERE user_id=?", (value, user_id))
+    cursor.execute(f"""
+    UPDATE users
+    SET {field}=?
+    WHERE user_id=?
+    """, (value, user_id))
     conn.commit()
