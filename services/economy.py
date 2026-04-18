9f2389a6854b1e40c7bbde484ae711e156cd68b8
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 21:23:27 +0300

    Update economy.py

--- services/economy.py
+++ services/economy.py
@@ -1,24 +1,2 @@
-import random
-
-def bonus_amount():
-    return random.randint(10, 25)
-
-
-def sell_value(rarity):
-    return {
-        "common": 5,
-        "rare": 15,
-        "epic": 40,
-        "legend": 100,
-        "myth": 0
-    }[rarity]
-
-
-def dust_value(rarity):
-    return {
-        "common": 2,
-        "rare": 6,
-        "epic": 15,
-        "legend": 40,
-        "myth": 0
-    }[rarity]
+def format_number(n):
+    return f"{n:,}".replace(",", " ")
