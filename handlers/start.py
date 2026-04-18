ac1faf368d1cd58c71fe3039b6549182795269aa
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 22:04:16 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,16 +1,14 @@
-from aiogram import Router, F
-from aiogram.types import Message
-from db import create_user
+from aiogram import Router, types
+from keyboards.main_menu import main_menu
 
 router = Router()
 
-
-@router.message(F.text == "/start")
-async def start(msg: Message):
-
-    await create_user(
-        msg.from_user.id,
-        msg.from_user.username
+@router.message(lambda m: m.chat.type == "private")
+async def start(msg: types.Message):
+    text = (
+        "👋 Добро пожаловать в Waifu World!\n\n"
+        "🎴 Собирай вайфу, открывай карточки и развивай коллекцию\n\n"
+        "👉 Нажми кнопку ниже или напиши 'карта'"
     )
 
-    await msg.answer("👋 Добро пожаловать в Waifu World")
+    await msg.answer(text, reply_markup=main_menu())
