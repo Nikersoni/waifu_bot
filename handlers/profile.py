3712375e9f3ee8613edc0671cb9b7fdb6886f7ea
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 13:35:01 +0300

    Update profile.py

--- handlers/profile.py
+++ handlers/profile.py
@@ -1,1 +1,21 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from db import get_user
 
+router = Router()
+
+@router.message(F.text.in_(["👤 Профиль", "профиль"]))
+async def profile(msg: Message):
+    user = get_user(msg.from_user.id)
+
+    if not user:
+        await msg.answer("Нет профиля")
+        return
+
+    await msg.answer(
+f"""👤 {msg.from_user.username}
+
+💎 Алмазы: {user[2]}
+🧪 Пыль: {user[3]}
+"""
+    )
