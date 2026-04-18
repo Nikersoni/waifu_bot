896aed38e18cfa13ef476635856da66cb9400402
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 13:35:30 +0300

    Update top.py

--- handlers/top.py
+++ handlers/top.py
@@ -1,1 +1,18 @@
+from aiogram import Router, F
+from aiogram.types import Message
+from db import cursor
 
+router = Router()
+
+@router.message(F.text == "🏆 Топ")
+async def top(msg: Message):
+
+    cursor.execute("SELECT username, diamonds FROM users ORDER BY diamonds DESC LIMIT 5")
+    rows = cursor.fetchall()
+
+    text = "🏆 ТОП АЛМАЗОВ\n\n"
+
+    for i, r in enumerate(rows, 1):
+        text += f"{i}. {r[0]} — {r[1]} 💎\n"
+
+    await msg.answer(text)
