cb7ce5a8c989c019054adcd68c36f1587d43811c
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 23:46:49 +0300

    Update inventory.py

--- handlers/inventory.py
+++ handlers/inventory.py
@@ -3,6 +3,6 @@ from aiogram import Router, types
 router = Router()
 
 
-@router.message(lambda m: m.text and m.text.lower().strip() in ["инв", "инвентарь", "🎒 инв"])
+@router.message(lambda m: m.text and m.text.lower().strip() in ["инв", "инвентарь", "🎒 инвентарь"])
 async def inv(msg: types.Message):
     await msg.answer("🚧 инвентарь в разработке")
