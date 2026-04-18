2ce02b530bece64b0624e240d7f15df604551117
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 22:46:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 BOT_TOKEN = "8648276698:AAEPegOqD3pThyls1Qx5A3TkIQAH90AMtoA"
 
-DB_HOST = "postgres"
-DB_NAME = "waifu"
+DB_HOST = "postgres.railway.internal"
+DB_PORT = 5432
+DB_NAME = "railway"
 DB_USER = "postgres"
-DB_PASS = "password"
+DB_PASS = "PUT_YOUR_DB_PASSWORD_HERE""
