58f300a6875276addbb09f0832cbafb24a1a743f
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 13:36:02 +0300

    Update institute.py

--- handlers/institute.py
+++ handlers/institute.py
@@ -1,1 +1,8 @@
+from aiogram import Router, F
+from aiogram.types import Message
 
+router = Router()
+
+@router.message(F.text.in_(["🏫 Институт", "институт"]))
+async def inst(msg: Message):
+    await msg.answer("🚧 Институт в разработке")
