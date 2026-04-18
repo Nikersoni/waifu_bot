fec162db604db03896f9de53d83f4c1a0d673a3e
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-19 00:34:36 +0300

    Update top.py

--- handlers/top.py
+++ handlers/top.py
@@ -6,3 +6,7 @@ router = Router()
 @router.message(lambda m: m.text and m.text.lower().strip() in ["топ", "🏆 топ"])
 async def top(msg: types.Message):
     await msg.answer("🚧 топ в разработке")
+    
+@router.message()
+async def test(msg: types.Message):
+    print("LS:", msg.text)
