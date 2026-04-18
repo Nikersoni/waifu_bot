2beafdd997189068c2b10d32873c041381c306fd
Snikers <78871547+Nikersoni@users.noreply.github.com>
2026-04-18 22:59:01 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,46 +1,26 @@
 import asyncpg
-from config import *
+from config import DATABASE_URL
 
-pool: asyncpg.Pool | None = None
+pool = None
 
 
 async def init_db():
     global pool
 
-    pool = await asyncpg.create_pool(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS,
-        min_size=1,
-        max_size=10
-    )
+    pool = await asyncpg.create_pool(DATABASE_URL)
 
-    await create_tables()
-
-
-async def get_pool():
-    return pool
-
-
-async def create_tables():
     async with pool.acquire() as conn:
 
-        # USERS TABLE
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
             user_id BIGINT PRIMARY KEY,
             username TEXT,
-
             diamonds BIGINT DEFAULT 0,
             dust BIGINT DEFAULT 0,
-
             streak INT DEFAULT 0,
             last_streak_date DATE,
-
             last_card TIMESTAMP,
             last_bonus TIMESTAMP,
-
             active_name TEXT,
             active_rarity TEXT,
             active_level INT DEFAULT 1,
@@ -48,20 +28,21 @@ async def create_tables():
         );
         """)
 
-        # INVENTORY TABLE
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS inventory (
             user_id BIGINT,
             card_name TEXT,
             rarity TEXT,
             count INT DEFAULT 1,
-
             PRIMARY KEY (user_id, card_name)
         );
         """)
 
 
-# 👤 АВТО-СОЗДАНИЕ ПОЛЬЗОВАТЕЛЯ
+async def get_pool():
+    return pool
+
+
 async def get_or_create_user(user_id: int, username: str):
     async with pool.acquire() as conn:
 
